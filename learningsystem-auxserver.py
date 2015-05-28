fd6cd22d442bebcb066f9c82788d712440e65887
Nikita Lyubchich <cybran111@gmail.com>
2015-05-28 12:37:01 +0300

    Initial

--- learningsystem-auxserver.py
+++ learningsystem-auxserver.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 from nltk.corpus import stopwords
 import requests
 import nltk
-
+nltk.download("stopwords")
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
