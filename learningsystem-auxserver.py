0620da55c42057f9666508fab9a2c693ce258d81
Nikita Lyubchich <cybran111@gmail.com>
2015-05-28 14:07:18 +0300

    Initial

--- learningsystem-auxserver.py
+++ learningsystem-auxserver.py
@@ -16,7 +16,8 @@ def hello_world():
         with archive.open("extra.txt") as solution:
             languages_ratios = {}
             tokens = nltk.wordpunct_tokenize(solution.read().decode('utf-8'))
-            words_set = set([word.lower() for word in tokens])
+            words_list = [word.lower() for word in tokens]
+            words_set = set(words_list)
             print "Words_set: ", words_set
             for language in stopwords.fileids():
                 stopwords_set = set(stopwords.words(language))
@@ -29,7 +30,7 @@ def hello_world():
             mark = 50 if max(languages_ratios, key=languages_ratios.get) == 'english' else 0
             # 50%
             print "Mark for lang: ", mark
-            words_count = len(words_set)
+            words_count = len(words_list)
             print "Words count: ", words_count
             mark += (float(words_count) / 200) * 50 if words_count < 200 else 50
             print "Total Mark: ", mark
